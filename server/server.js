838e468060b5e4132338fe5d385de3fc47c174e0
Snehal Shah <snehalshahdev@gmail.com>
2018-12-19 11:35:35 +0530

    add generateMessage method

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/utils.js');
 const port = process.env.PORT || 2000;
 var app = express();
 var server = http.createServer(app);
@@ -9,22 +10,14 @@ var io = socketIO(server);
 app.use(express.static(path.join(__dirname, "../public")));
 io.on('connection', (socket) => {
   console.log("New client Connected");
-  socket.emit('newMessage', {
-    from : "Admin",
-    text : "Welcome to the chat app",
-    createdAt : new Date().getTime()
-  });
-  socket.broadcast.emit('newMessage', {
-    from : "Admin",
-    text : "New User connected",
-    createdAt : new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage("Admin", "Welcome to the chat app"));
+  socket.broadcast.emit('newMessage', generateMessage("Admin", "New client connected"));
   socket.on('disconnect', () => {
     console.log("client disconnected");
   });
   socket.on('createMessage', (msg) => {
     console.log('Message : ',msg);
-    io.emit('newMessage', {from : msg.from, text : msg.text, createdAt : new Date().getTime()});
+    io.emit('newMessage', generateMessage(msg.from, msg.text));
   });
 
 });
