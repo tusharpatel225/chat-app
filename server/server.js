f0270ad253ae7869874dc1d664159155fdb441db
Snehal Shah <snehalshahdev@gmail.com>
2018-12-19 10:13:47 +0530

    create and receive message

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const port = process.env.PORT || 2000;
-
 var app = express();
-
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(path.join(__dirname, "../public")));
+io.on('connection', (socket) => {
+  console.log("New client Connected");
 
-app.listen(port, () => {
+  socket.emit('newMessage', {from : "Tushar", text : "Hello", createdAt : new Date().getTime()});
+
+  socket.on('disconnect', () => {
+    console.log("client disconnected");
+  });
+  socket.on('createMessage', (msg) => {
+    console.log('Message : ',msg);
+  });
+
+});
+
+server.listen(port, () => {
   console.log("server start at ",port);
 });
