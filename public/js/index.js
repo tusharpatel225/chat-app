1066eccee67c194196ec705ab546af2933721d93
Snehal Shah <snehalshahdev@gmail.com>
2018-12-20 13:31:58 +0530

    add usename and timestamp

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,13 @@
+var c = document.getElementById("chat");
+var user = document.getElementById("u");
+user.style.visibility = "hidden";
 var socket = io();
 var ol = document.getElementById('msgList');
+if(sessionStorage.getItem("userName")=="")
+{
+        user.style.visibility = "visible";
+        c.style.visibility = "hidden";
+}
 socket.on('connect', () => {
   console.log("Connected to Server");
 });
@@ -8,17 +16,19 @@ socket.on('disconnect', () => {
 });
 socket.on('newMessage', (msg) => {
   var li = document.createElement('li');
-  var text = document.createTextNode(msg.from+" : "+msg.text);
+    var time = moment(msg.createdAt).format("hh:mm a");
+  var text = document.createTextNode(`${msg.from} ${time} : ${msg.text}`);
   li.appendChild(text);
   ol.appendChild(li);
 });
 socket.on('newLocation', (msg) => {
+    var time = moment(msg.createdAt).format("hh:mm a");
   var li = document.createElement('li');
     var a = document.createElement('a');
     a.href = msg.mapURL;
     a.target = "_blank";
     a.appendChild(document.createTextNode("My location"));
-      li.appendChild(document.createTextNode(msg.from+" : "));
+      li.appendChild(document.createTextNode(`${msg.from} ${time} : `));
   li.appendChild(a);
   ol.appendChild(li);
 });
@@ -26,8 +36,11 @@ function msgSend()
 {
   var msg = document.getElementById('txtMsg');
     if(msg.value.length==0)
+    {
+        msg.focus();
         return false;
-    socket.emit('createMessage', {from : "Tushar", text : msg.value}, (data) => {
+    }
+    socket.emit('createMessage', {from : sessionStorage.getItem('userName'), text : msg.value}, (data) => {
       msg.value="";
     console.log("From server : ",data);
   });
@@ -40,13 +53,15 @@ function sendLocation()
     {
         navigator.geolocation.getCurrentPosition((position) => {
             btn.disabled = true;
+            btn.value = "sending location...";
            socket.emit('createLocation', {
-               from : "Tushar",
+               from : sessionStorage.getItem('userName'),
                lat : position.coords.latitude,
                lon : position.coords.longitude
            }
         , (data) => {
             console.log("From server : ",data); 
+            btn.value="Send Location";
             btn.disabled = false;
            });
         }, () => {
@@ -58,5 +73,17 @@ function sendLocation()
         return alert("Geo location is not available");
     }
 
+}
+function setUser()
+{
+  var u = document.getElementById('userName');
+    if(u.value.length==0)
+    {
+        u.focus();
+        return false;
+    }
+    sessionStorage.setItem("userName", u.value);
+    c.style.visibility = "visible";
+    user.style.visibility = "hidden";
 }
 
--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,15 +1,16 @@
+var moment = require('moment');
 var generateMessage = (from, text) => {
   return {
     from,
     text,
-    createdAt : new Date().getTime()
+    createdAt : moment().valueOf()
   };
 };
 var generateLocation = (from, lat, lon) => {
   return {
     from,
     mapURL : `https://www.google.com/maps?q=${lat},${lon}`,
-    createdAt : new Date().getTime()
+    createdAt : moment().valueOf()
   };
 };
 
